69fd8e6a79a3e370a4bac5c73678067b3f9c1856
Ote Ian <oteian9@gmail.com>
2024-01-07 22:36:07 +0300

    Fix: Documentation

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -12,6 +12,14 @@ from api.v1.views import app_views
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
 def list_amenity_objects():
     """
+    Retrieve a list of all Amenity objects.
+
+    Returns:
+    - JSON representation of the list of Amenity objects.
+
+    Example:
+    $ curl http://127.0.0.1:5000/api/v1/amenities
+    Output: JSON representation of all Amenity objects
     """
     all_obj = storage.all(Amenity)
     return jsonify([obj.to_dict() for obj in all_obj.values()])
@@ -21,6 +29,17 @@ def list_amenity_objects():
                  strict_slashes=False)
 def get_amenity_object(amenity_id):
     """
+    Retrieve a specific Amenity object based on its ID.
+
+    Parameters:
+    - amenity_id: The ID of the Amenity object to retrieve.
+
+    Returns:
+    - JSON representation of the specified Amenity object.
+
+    Example:
+    $ curl http://127.0.0.1:5000/api/v1/amenities/1
+    Output: JSON representation of the Amenity object with ID 1
     """
     obj = storage.get(Amenity, amenity_id)
     if not obj:
@@ -33,6 +52,17 @@ def get_amenity_object(amenity_id):
                  strict_slashes=False)
 def delete_amenity_object(amenity_id):
     """
+    Delete a specific Amenity object based on its ID.
+
+    Parameters:
+    - amenity_id: The ID of the Amenity object to delete.
+
+    Returns:
+    - Empty JSON response with HTTP status code 200 upon successful deletion.
+
+    Example:
+    $ curl -X DELETE http://127.0.0.1:5000/api/v1/amenities/1
+    Output: {}
     """
     obj = storage.get(Amenity, amenity_id)
     if not obj:
@@ -48,6 +78,16 @@ def delete_amenity_object(amenity_id):
 @app_views.route('/amenities', methods=['POST'], strict_slashes=False)
 def create_amenity_object():
     """
+    Create a new Amenity object.
+
+    Returns:
+    - JSON representation of the newly created Amenity object.
+    - HTTP status code 201 upon successful creation.
+
+    Example:
+    $ curl -X POST -H "Content-Type: application/json" \
+      -d '{"name": "New Amenity"}' http://127.0.0.1:5000/api/v1/amenities
+    Output: JSON representation of the newly created Amenity object
     """
     data = request.get_json()
     if not data:
@@ -66,6 +106,19 @@ def create_amenity_object():
                  strict_slashes=False)
 def update_amenity_object(amenity_id):
     """
+    Update a specific Amenity object based on its ID.
+
+    Parameters:
+    - amenity_id: The ID of the Amenity object to update.
+
+    Returns:
+    - JSON representation of the updated Amenity object.
+    - HTTP status code 200 upon successful update.
+
+    Example:
+    $ curl -X PUT -H "Content-Type: application/json" \
+      -d '{"name": "Updated Amenity"}' http://127.0.0.1:5000/api/v1/amenities/1
+    Output: JSON representation of the updated Amenity object
     """
     obj = storage.get(Amenity, amenity_id)
     if not obj or obj is None:
