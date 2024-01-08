51f912512767ce9e1a939ced60fd4423a6ba036d
MarkMusili <musiliyrn@gmail.com>
2024-01-08 17:39:57 +0300

    lets see

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,6 +8,12 @@ The route returns a JSON response indicating the status as "OK"
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -30,11 +36,11 @@ def show_data():
     Retrieves the number of each objects by type.
     """
     stats = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
     }
     return jsonify(stats), 200
