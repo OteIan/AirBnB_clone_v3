126256be26ed4e75c9d023edf5949b51367a67b7
MarkMusili <musiliyrn@gmail.com>
2024-01-08 17:34:31 +0300

    CORS implimentation

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,11 +10,14 @@ The application is configured to handle the endpoint '/status'
 """
 from api.v1.views import app_views
 from flask import Flask
+from flask_cors import CORS
 from models import storage
 from flask import jsonify
 import os
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 app.register_blueprint(app_views)
 
 
