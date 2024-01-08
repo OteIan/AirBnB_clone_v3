394942486197bf280935dc3595c4aada9bf5061e
Ote Ian <oteian9@gmail.com>
2024-01-08 15:45:42 +0300

    Fix: Format change

--- api/v1/views/place_reviews.py
+++ api/v1/views/place_reviews.py
@@ -11,87 +11,149 @@ from flask import abort, jsonify, request, make_response
 from api.v1.views import app_views
 
 
-@app_views.route('/places/<place_id>/reviews', methods=['GET'], strict_slashes=False)
-def list_review_objects(place_id):
-    """
-    """
-    place = storage.get(Place, place_id)
-    if not place:
-        abort(404)
+# @app_views.route('/places/<place_id>/reviews', methods=['GET'], strict_slashes=False)
+# def list_review_objects(place_id):
+#     """
+#     """
+#     place = storage.get(Place, place_id)
+#     if not place:
+#         abort(404)
 
-    all_objs = storage.all(Review)
-    return jsonify([obj.to_dict() for obj in all_objs.values() if obj.place_id == place_id])
-
-
-@app_views.route('/reviews/<review_id>', methods=['GET'], strict_slashes=False)
-def get_review_objects(review_id):
-    """
-    """
-    obj = storage.get(Review, review_id)
-    if not obj:
-        abort(404)
-    return jsonify(obj.to_dict())
+#     all_objs = storage.all(Review)
+#     return jsonify([obj.to_dict() for obj in all_objs.values() if obj.place_id == place_id])
 
 
-@app_views.route('/reviews/<review_id>', methods=['DELETE'],
-                 strict_slashes=False)
-def delete_review_objects(review_id):
-    """
+# @app_views.route('/reviews/<review_id>', methods=['GET'], strict_slashes=False)
+# def get_review_objects(review_id):
+#     """
+#     """
+#     obj = storage.get(Review, review_id)
+#     if not obj:
+#         abort(404)
+#     return jsonify(obj.to_dict())
+
+
+# @app_views.route('/reviews/<review_id>', methods=['DELETE'],
+#                  strict_slashes=False)
+# def delete_review_objects(review_id):
+#     """
     
-    """
-    obj = storage.get(Review, review_id)
-    if obj is None or not obj:
-        abort(404)
+#     """
+#     obj = storage.get(Review, review_id)
+#     if obj is None or not obj:
+#         abort(404)
 
-    obj.delete()
-    storage.save()
+#     obj.delete()
+#     storage.save()
 
-    return make_response(jsonify({}), 200)
+#     return make_response(jsonify({}), 200)
 
 
-@app_views.route('/places/<place_id>/reviews', methods=['POST'], strict_slashes=False)
-def create_review(place_id):
+# @app_views.route('/places/<place_id>/reviews', methods=['POST'], strict_slashes=False)
+# def create_review(place_id):
+#     """
+#     """
+#     place = storage.get(Place, place_id)
+#     if not place:
+#         abort(404)
+
+#     data = request.get_json()
+#     if not data:
+#         abort(400, "Not a JSON")
+#     elif 'user_id' not in data.keys():
+#         abort(400, 'Missing user_id')
+#     elif 'text' not in data.keys():
+#         abort(400, 'Missing text')
+
+#     user = storage.get(User, data['user_id'])
+#     if not user:
+#         abort(404)
+
+#     obj = Review(place_id=place_id, **data)
+#     storage.new(obj)
+#     storage.save()
+
+#     return make_response(jsonify(obj.to_dict()), 201)
+
+
+# @app_views.route('/reviews/<review_id>', methods=['PUT'], strict_slashes=False)
+# def update_review_objects(review_id):
+#     """
+#     """
+#     obj = storage.get(Review, review_id)
+#     if obj is None or not obj:
+#         abort(404)
+
+#     data = request.get_json()
+#     if not data:
+#         abort(404, "Not a JSON")
+
+#     for key, value in data.items():
+#         if key not in ['id', 'created_at', 'updated_at', 'user_id', 'place_id']:
+#             setattr(obj, key, value)
+
+#     storage.save()
+
+#     return make_response(jsonify(obj.to_dict()), 200)
+
+
+
+@app_views.route('/places/<place_id>/reviews', methods=['GET', 'POST'])
+def get_reviews(place_id):
     """
     """
     place = storage.get(Place, place_id)
     if not place:
         abort(404)
 
-    data = request.get_json()
-    if not data:
-        abort(400, "Not a JSON")
-    elif 'user_id' not in data.keys():
-        abort(400, 'Missing user_id')
-    elif 'text' not in data.keys():
-        abort(400, 'Missing text')
+    if request.method == 'GET':
+        all_objs = storage.all(Review)
+        return jsonify([obj.to_dict() for obj in all_objs.values() if obj.place_id == place_id])
+    
+    if request.method == 'POST':
+        data = request.get_json()
+        if not data:
+            abort(400, "Not a JSON")
+        elif 'user_id' not in data.keys():
+            abort(400, 'Missing user_id')
+        elif 'text' not in data.keys():
+            abort(400, 'Missing text')
 
-    user = storage.get(User, data['user_id'])
-    if not user:
-        abort(404)
+        user = storage.get(User, data['user_id'])
+        if not user:
+            abort(404)
 
-    obj = Review(place_id=place_id, **data)
-    storage.new(obj)
-    storage.save()
+        obj = Review(place_id=place_id, **data)
+        storage.new(obj)
+        storage.save()
 
-    return make_response(jsonify(obj.to_dict()), 201)
+        return make_response(jsonify(obj.to_dict()), 201)
 
 
-@app_views.route('/reviews/<review_id>', methods=['PUT'], strict_slashes=False)
-def update_review_objects(review_id):
+@app_views.route('/reviews/<review_id>', methods=['PUT', 'GET', 'DELETE'])
+def method_reviews(review_id):
     """
     """
     obj = storage.get(Review, review_id)
     if obj is None or not obj:
         abort(404)
 
-    data = request.get_json()
-    if not data:
-        abort(404, "Not a JSON")
+    if request.method == 'GET':
+        return jsonify(obj.to_dict())
+    
+    if request.method == 'DELETE':
+        obj.delete()
+        storage.save()
+        return jsonify({}), 200
+    
+    if request.method == 'PUT':
+        data = request.get_json()
+        if not data:
+            abort(404, "Not a JSON")
 
-    for key, value in data.items():
-        if key not in ['id', 'created_at', 'updated_at', 'user_id', 'place_id']:
-            setattr(obj, key, value)
+        for key, value in data.items():
+            if key not in ['id', 'created_at', 'updated_at', 'user_id', 'place_id']:
+                setattr(obj, key, value)
 
-    storage.save()
-
-    return make_response(jsonify(obj.to_dict()), 200)
+        storage.save()
+        return jsonify(obj.to_dict()), 200
